eb0d63894bf2e0e2f4f0ffa898df2e9e388a4390
Olushola <olushola@Users-MacBook-Pro.local>
2019-08-28 15:52:32 +0100

    test complete

---
+++ test.js
@@ -0,0 +1,31 @@
+const list = [1, 3, 5, 2]
+
+function getSmallestNumberOfSwapsPossible (list=[]) {
+    let arr = [...list]
+    arr.sort()
+    let swaps= 0
+    let min = Math.min(...list)
+    let current_pos = 0
+
+    while(arr.length > 0) {
+        let min_pos = list.indexOf(min)
+
+        if (min_pos !== current_pos) {
+            [list[min_pos], list[current_pos]] = [list[current_pos], list[min_pos]]
+            swaps++
+        } 
+
+        arr = arr.filter((item) => {
+            return item !== min
+        })
+
+        min = Math.min(...arr)
+        current_pos++
+    }
+
+    return {swaps, list}
+}
+
+getSmallestNumberOfSwapsPossible(list) // should be 2
+
+
